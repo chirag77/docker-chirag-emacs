f73ed23683597774c02c4aff4e6c16fb769446a0
Chirag <chiragconnect6174@gmail.com>
2020-08-22 22:33:41 +0530

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; this file is user-init-file
+(setq user-init-file (or load-file-name (buffer-file-name)))
+;; user-emacs-directory is defined in subr.el , which is same as this.
+(setq user-emacs-directory (file-name-directory user-init-file))
+
+
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
